41359dc74ce9bf12048f97637ccef80d91d169ef
devashrlucas <devashrlucas@gmail.com>
2020-09-22 11:06:04 -0400

    Added way to create database in connect.py.

--- src/scripts/connect.py
+++ src/scripts/connect.py
@@ -1,3 +1,13 @@
+'''
+For Mac users: please remove the '.DS_Store' file from the main directory.
+If you have already done so globally, you can ignore the following set of instructions.
+1) Open terminal
+2) In command line, run: cd /path/to/OIET_DATA_DB
+3) In command line, run: find . -name '.DS_Store' -type f -delete
+'''
+
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
 import psycopg2
 from psycopg2 import OperationalError
 from dotenv import load_dotenv
@@ -12,6 +22,9 @@ DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
 DATABASE_HOST = os.getenv("DATABASE_HOST")
 DATABASE_PORT = os.getenv("DATABASE_PORT")
 
+engine = create_engine("postgres://localhost/oiet_data")
+if not database_exists(engine.url):
+    create_database(engine.url)
 
 try:
     connection=psycopg2.connect(
